e91aa55aa47e92925acea52ac982c8ad2415ccd8
Vebriclya <135071199+Vebriclya@users.noreply.github.com>
2024-01-07 22:39:30 +0000

    Finished up request portion, pushing before I move all my HTML over to request.html

--- server.js
+++ server.js
@@ -17,15 +17,22 @@ app.get("/users", async (req, res) => {
     { id: 3, name: "Shannon Jackson" },
   ]; */
 
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users = await response.json();
+  /* The surrounding setTimeout can be removed entirely, it's just to mimic a slow server! */
+  setTimeout(async () => {
+    const limit = +req.query.limit || 10;
 
-  res.send(`
-   <h1 class="text-2xl font-bold my-4">Users</h1>
-   <ul>
-    ${users.map((user) => `<li>${user.name}</li>`).join("")}
-   </ul>
-  `);
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/users?_limit=${limit}`
+    );
+    const users = await response.json();
+
+    res.send(`
+     <h1 class="text-2xl font-bold my-4">Users</h1>
+     <ul>
+      ${users.map((user) => `<li>${user.name}</li>`).join("")}
+     </ul>
+    `);
+  }, 2000);
 });
 
 // Start the server
